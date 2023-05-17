8e7766d3c273b6a4b59d71ea9c51baa13a3c7c45
Alessia <98785381+amorea9@users.noreply.github.com>
2023-05-17 18:04:34 +0200

    deleting extra div tag ops

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -190,17 +190,13 @@ export default function Home() {
           <Radio label="I'm an option" value="Option1" defaultChecked={true} fieldName="radio" />
           <Radio label="So am I, pick me" value="Option2" fieldName="radio" />
         </div>
-      </div>
-      <ProductCard productName={"Teddy"} price={1200} productImage={"https://omhucph.com/wp-content/uploads/2023/04/DSC_9254_MBS-5769-Cream-white_chrome_square-1.jpg"} newlyAdded={true} colors={hexCodesArray} />
-      <div className="flex flex-row justify-between">
-        <FilterMenu handleFilter={handleFilter} handlePriceChange={handlePriceChange} />
-        <SortMenu handleSort={handleSort} />
-      </div>
-  
 
+        <ProductCard productName={"Teddy"} price={1200} productImage={"https://omhucph.com/wp-content/uploads/2023/04/DSC_9254_MBS-5769-Cream-white_chrome_square-1.jpg"} newlyAdded={true} colors={hexCodesArray} />
+        <div className="flex flex-row justify-between">
+          <FilterMenu handleFilter={handleFilter} handlePriceChange={handlePriceChange} />
+          <SortMenu handleSort={handleSort} />
+        </div>
       </Layout>
     </>
-
-
   );
 }
