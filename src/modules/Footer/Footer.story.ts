259470da53ce25aebab33c5ee7ad58610f394a39
Maria Lundqvist <mari28rd@stud.kea.dk>
2023-05-17 15:09:35 +0200

    set up footer with content and links, styled for mobile

---
+++ src/modules/Footer/Footer.story.ts
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Footer from "./Footer";
+
+type Story = StoryObj<typeof Footer>;
+
+export default {
+  component: Footer,
+} satisfies Meta<typeof Footer>;
+
+export const Default: Story = {};
