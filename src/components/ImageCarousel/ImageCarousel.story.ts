8921688d53ad5089f8916680bd4f7167634aaf4f
Maria Lundqvist <mari28rd@stud.kea.dk>
2023-05-19 12:27:22 +0200

    image carousel set up and styling done

---
+++ src/components/ImageCarousel/ImageCarousel.story.ts
@@ -0,0 +1,46 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ImageCarousel from "./ImageCarousel";
+import measurements from "../../assets/mesurments_teddyFolded.svg";
+
+type Story = StoryObj<typeof ImageCarousel>;
+
+export default {
+  component: ImageCarousel,
+} satisfies Meta<typeof ImageCarousel>;
+
+export const Default: Story = {
+  args: {
+    images: [
+      {
+        imageSrc: "https://images.unsplash.com/photo-1567016432779-094069958ea5",
+        imageWidth: 900,
+        imageHeight: 900,
+        alt: "Sofa",
+      },
+      {
+        imageSrc: "https://images.unsplash.com/photo-1615800001964-5afd0ae8e49a",
+        imageWidth: 852,
+        imageHeight: 1279,
+        alt: "Sofa",
+      },
+      {
+        imageSrc: "https://images.unsplash.com/photo-1555041469-a586c61ea9bc",
+        imageWidth: 946,
+        imageHeight: 630,
+        alt: "Sofa",
+      },
+      {
+        imageSrc: "https://images.unsplash.com/photo-1567016507665-356928ac6679",
+        imageWidth: 946,
+        imageHeight: 946,
+        alt: "Sofa",
+      },
+      {
+        imageSrc: "https://images.unsplash.com/photo-1569813017258-b9bbf395b008",
+        imageWidth: 946,
+        imageHeight: 1261,
+        alt: "Sofa",
+      },
+    ],
+  },
+};
---
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Thumbs } from "swiper";
+import Image from "next/image";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+import "../../../styles/globals.css";
+
+type Image = {
+  imageSrc: string;
+  imageWidth: number;
+  imageHeight: number;
+  alt?: string;
+};
+
+interface ImageCarouselProps {
+  images: Image[];
+}
+
+function ImageCarousel({ images }: ImageCarouselProps) {
+  const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
+  return (
+    <div className="space-y-2">
+      <Swiper loop={true} spaceBetween={10} navigation={true} thumbs={{ swiper: thumbsSwiper }} modules={[FreeMode, Navigation, Thumbs]} className="carousel">
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <Image src={image.imageSrc} width={image.imageWidth} height={image.imageHeight} alt={image?.alt ? image.alt : ""} className="aspect-square" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Swiper onSwiper={setThumbsSwiper} loop={true} spaceBetween={10} slidesPerView={5} freeMode={true} watchSlidesProgress={true} modules={[FreeMode, Navigation, Thumbs]} className="thumbnails">
+        {images.map((image, index) => (
+          <SwiperSlide key={`${index}-thumbnail`}>
+            <Image src={image.imageSrc} width={image.imageWidth} height={image.imageHeight} alt={image?.alt ? image.alt : ""} className="aspect-square" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default ImageCarousel;
