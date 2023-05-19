a3b30683807b7cab80f1eec400849f7eb38df369
Alessia <98785381+amorea9@users.noreply.github.com>
2023-05-19 18:33:10 +0200

    product card is clickable + colors fix

--- src/components/Cards/ProductCard.story.ts
+++ src/components/Cards/ProductCard.story.ts
@@ -13,6 +13,23 @@ export const Default: Story = {
     price: 1200,
     productImage: "https://omhucph.com/wp-content/uploads/2023/04/DSC_9254_MBS-5769-Cream-white_chrome_square-1.jpg",
     newlyAdded: true,
-    colors: ["#000000", "#dddddd", "#ff0000", "#00ff00", "#0000ff", "#0000ff", "#00ff00", "#0000ff", "#0000ff", "#dddddd", "#ff0000", "#00ff00", "#0000ff", "#0000ff", "#00ff00"],
+    colors: {
+      brown: {
+        label: "Brown",
+        value: "765",
+        hexCode: "#60342C",
+        images: ["https://amorea.dk/exam/finalExam/ProductPictures/corner/brown.jpg", "https://amorea.dk/exam/finalExam/ProductPictures/corner/brown_closeup.jpg", "https://amorea.dk/exam/finalExam/ProductPictures/corner/brown_full.jpg"],
+      },
+      cream_white: {
+        label: "Cream White",
+        value: "765",
+        hexCode: "#EEE6E4",
+        images: [
+          "https://amorea.dk/exam/finalExam/ProductPictures/corner/cream_white.jpg",
+          "https://amorea.dk/exam/finalExam/ProductPictures/corner/cream_white_closeup.jpg",
+          "https://amorea.dk/exam/finalExam/ProductPictures/corner/cream_white_full.jpg",
+        ],
+      },
+    },
   },
 };
