30775599f25280952f90e18ab01a43b6784c97f6
Maria Lundqvist <mari28rd@stud.kea.dk>
2023-05-19 14:45:39 +0200

    created and styled video module with thumbnail and vimeo compatibility

---
+++ src/modules/Video/Video.story.ts
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Video from "./Video";
+
+type Story = StoryObj<typeof Video>;
+
+export default {
+  component: Video,
+} satisfies Meta<typeof Video>;
+
+export const Default: Story = {
+  args: {
+    title: "Test title",
+    videoSrc: "https://vimeo.com/783453158",
+    videoThumbnail: {
+      imageSrc: "https://images.unsplash.com/photo-1571838475231-bb5aa087c797",
+      alt: "video thumbnail",
+    },
+  },
+};
+
+export const NoThumbnail: Story = {
+  args: {
+    title: "Test title",
+    videoSrc: "https://vimeo.com/783453158",
+  },
+};
