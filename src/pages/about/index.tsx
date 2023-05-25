8a3a51dd3f9fd060411fc2d3af175862dad86188
Maria Lundqvist <mari28rd@stud.kea.dk>
2023-05-25 16:49:25 +0200

    ABOUT PAGE IS DONE CAN YOU BELIEVE IT

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Head from "next/head";
+import { Layout } from "@/app/pageLayout";
+import AboutHero from "@/modules/AboutHero/AboutHero";
+import AboutTimeline from "@/modules/AboutTimeline/AboutTimeline";
 
 function about() {
   return (
-    <div>
-      <Head>
-        <title>Omhu | About</title>
-      </Head>
-    </div>
+    <Layout>
+      <div className="pt-10 lg:pt-20">
+        <AboutHero />
+      </div>
+      <div className="mr-5 lg:mr-0">
+        <AboutTimeline />
+      </div>
+    </Layout>
   );
 }
 
