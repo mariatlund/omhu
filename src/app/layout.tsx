14a247a220ec0120d52e1a055a6c43354a892556
Alessia <98785381+amorea9@users.noreply.github.com>
2023-05-30 13:23:28 +0200

    uncommented metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,62 +3,28 @@ import "../../styles/globals.css";
 import Navigation from "@/components/Menu/Navigation";
 import Footer from "@/modules/Footer/Footer";
 import React from "react";
+import { Metadata } from "next";
 // import favicon from "../../public/favicon.png";
 
 // _APP.JS - ADD LOGIC HERE
 
-// export const metadata: Metadata = {
-//   title: {
-//     default: "Furniture with care | OMHU",
-//     template: "%s | OMHU",
-//   },
-//   keywords: ["Sofa", "Furniture", "Decor", "Furniture design", "Retro furniture", "Teddy"],
-//   description: "Your home, with omhu.",
-//   robots: {
-//     index: false,
-//     follow: false,
-//   },
-//   icons: {
-//     icon: "../assets/favicon.png",
-//   },
-// };
+export const metadata: Metadata = {
+  title: {
+    default: "Furniture with care | OMHU",
+    template: "%s | OMHU",
+  },
+  keywords: ["Sofa", "Furniture", "Decor", "Furniture design", "Retro furniture", "Teddy"],
+  description: "Your home, with omhu.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+  icons: {
+    icon: "../assets/favicon.png",
+  },
+};
 
 export default function RootLayout({ children }: { children?: React.ReactNode }) {
-  // const [orderInfo, setOrderInfo] = useState<OrderInfoType>({
-  //   total: 0,
-  //   items: [
-  //     {
-  //       image: "",
-  //       productName: "",
-  //       fabricColor: "",
-  //       fabricHex: "",
-  //       frameColor: "",
-  //       frameHex: "",
-  //       amount: 1,
-  //       price: 1000,
-  //     },
-  //   ],
-  // });
-
-  // const addToBasket = (product: any) => {
-  //   console.log("add to basket");
-  //   setOrderInfo((prevOrderInfo: OrderInfoType) => ({
-  //     ...prevOrderInfo,
-  //     items: [
-  //       ...prevOrderInfo.items,
-  //       {
-  //         image: product.image,
-  //         productName: product.name,
-  //         fabricColor: product.chosenFabricColor,
-  //         fabricHex: product.fabricHex,
-  //         frameColor: product.chosenMetalColor,
-  //         frameHex: product.metalHex,
-  //         amount: product.quantity,
-  //         price: product.price,
-  //       },
-  //     ],
-  //   }));
-  // };
   return (
     <html lang="en">
       <Head>
@@ -70,16 +36,6 @@ export default function RootLayout({ children }: { children?: React.ReactNode })
         <Navigation />
         <main id="main" className="text-blue">
           {children}
-
-          {/* {React.Children.map(children, (child) => {
-            if (React.isValidElement(child)) {
-              return React.cloneElement(child, {
-                addToBasket: addToBasket,
-              });
-            }
-            return null;
-          })} */}
-          {/* <Component addToBasket={addToBasket} /> */}
         </main>
         <Footer />
       </body>
